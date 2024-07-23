274e7b712fc3d22d21a5cc4affeebd5fa609d904
kartikeya700 <kartikeyamittal@outlook.com>
2024-07-23 12:49:03 +0530

    Update Rock Paper Scissors.py

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -26,20 +26,21 @@ def main():
 '''
     choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
     Choice = [rock, paper, scissors]
-    print(Choice[choice])
     computer = random.randint(0,2)
-    print("Computer chose: \n")
-    print(Choice[computer])
     if choice >= 3 or choice < 0:
         print("You enetred an invalid number, please try again.")
-    elif choice == 0 and computer == 2:
-        print("You win!")
-    elif computer == 0 and choice == 2:
-        print("You lose")
-    elif computer > choice:
-        print("You lose")
-    elif choice > computer:
-        print("You win!")
-    elif choice == computer:
-        print("It's a draw")        
+    else:
+        print("Computer chose: \n")
+        print(Choice[computer])
+        print(Choice[choice])
+        elif choice == 0 and computer == 2:
+            print("You win!")
+        elif computer == 0 and choice == 2:
+            print("You lose")
+        elif computer > choice:
+            print("You lose")
+        elif choice > computer:
+            print("You win!")
+        elif choice == computer:
+            print("It's a draw")        
 main()
